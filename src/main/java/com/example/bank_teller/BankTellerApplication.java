94e12a2331fb15b8dc032f978daff219315ef826
Nabihah01 <nabihah.rashid@rutgers.edu>
2022-03-19 00:22:42 -0400

    changed name of fxml

--- src/main/java/com/example/bank_teller/BankTellerApplication.java
+++ src/main/java/com/example/bank_teller/BankTellerApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class BankTellerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BankTellerApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BankTellerApplication.class.getResource("bankTeller-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Bank Teller");
         stage.setScene(scene);
